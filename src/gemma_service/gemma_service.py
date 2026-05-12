a77e60bc3da01e56447551eddc9e96507bc37340
chandanreddy10 <chandanreddy10@outlook.com>
2026-05-12 15:05:41 +0000

    update gemma service

--- src/gemma_service/gemma_service.py
+++ src/gemma_service/gemma_service.py
@@ -24,7 +24,7 @@ class LLMRequest(BaseModel):
     user_request: str
 
 
-def run_gemma_phrase(prompt: str):
+def run_gemma(prompt: str, request:str):
     start = time.time()
 
     response = chat(
@@ -32,23 +32,15 @@ def run_gemma_phrase(prompt: str):
     )
 
     end = time.time()
-    list_of_words = response.message.content.split(",")
-    list_of_words.append("End")
-    return {
-        "response": list_of_words,
-        "latency_ms": round((end - start) * 1000, 2),
-    }
-
-def run_gemma_sentence(prompt: str):
-    start = time.time()
-
-    response = chat(
-        model="gemma4:e2b", messages=[{"role": "user", "content": prompt}], stream=False
-    )
-
-    end = time.time()
-
-    return {
+    if request == "phrase":
+        list_of_words = response.message.content.split(",")
+        list_of_words.append("End")
+        return {
+            "response": list_of_words,
+            "latency_ms": round((end - start) * 1000, 2),
+        }
+    else:
+        return {
         "response": response.message.content,
         "latency_ms": round((end - start) * 1000, 2),
     }
@@ -66,8 +58,12 @@ def gemma_endpoint(req: LLMRequest):
     print(text)
     if user_request.lower() == "phrase":
         input_to_gemma = f"{phrase_prompt}\nList of Words selected by the patient : {text}"
-        return run_gemma_phrase(input_to_gemma)
+        return run_gemma(input_to_gemma, user_request.lower())
     
     elif user_request.lower() == "sentence":
         input_to_gemma = f"{sentence_prompt}\n Words to build the sentence from : {text}"
-        return run_gemma_sentence(input_to_gemma)
+        return run_gemma(input_to_gemma, user_request.lower())
+    
+    elif user_request.lower() == "story":
+        input_to_gemma = f"Build a short story from the Genre 10 lines. Gnere:\n{text}"
+        return run_gemma(input_to_gemma, user_request.lower())
